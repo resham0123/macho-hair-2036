1999e350c4ee1f5ebce86d80ef4f1b5b7a7a575d
kirtisule <kirtisule27111996@gmail.com>
2023-02-22 22:48:20 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+function first(){
+    document.querySelector(".slide>img").src="https://www.bigbasket.com/media/uploads/banner_images/hp_m_FMCG-PL_TastiesOrigins_460px-250123.jpg"
+ }
+ function second(){
+    document.querySelector(".slide>img").src="https://www.bigbasket.com/media/uploads/banner_images/hp_m_bcd_paneer_460px-020122.jpg"
+ }
+ function third(){
+    document.querySelector(".slide>img").src="	https://www.bigbasket.com/media/uploads/banner_images/hp_m_FMCG-PL_iDFreshoStore_460px-250123.jpg"
+ }
+ setInterval(first,2000);
+ setInterval(second,4000);
+ setInterval(third,6000);
+
