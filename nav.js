646abfe305fbfcad700c1bf8bdb95e7f2362719b
Mahendra <mahendrac121@gmail.com>
2023-02-24 14:32:59 +0530

    added some footer and header basic

---
+++ nav.js
@@ -0,0 +1,29 @@
+let nav = 
+`
+<div id="nav">
+<div>
+<img src="logo.jpeg" alt="">
+</div>
+<div id="search">
+<form action="" class="search_bar">
+<input type="text" placeholder="search for product" >
+<button type="submit"><img src="https://cdn-icons-png.flaticon.com/512/122/122932.png" alt=""></button>
+</form>
+  </div>
+  <div id="login">
+   <div>    
+    <a href="signup.html" id="rsign" >Login/SignUp</a>
+    <dialog>
+       <h1></h1>
+    </dialog>
+   </div>
+   <div>
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU" alt="">
+       <h3>My Basket</h3>
+       <h3 id="cart_count">0</h3>
+   </div>
+</div>
+</div>
+`
+
+export default nav
