3d1e7f862bfe22a9c8ca5aff9952391e10c35cbb
vamshikrishna4992 <112753841+vamshikrishna4992@users.noreply.github.com>
2023-02-26 20:25:04 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,19 +2,12 @@ let nav =
 
 `
 <div>
-<a href="./index.html"><img id="logo" src="logo.jpeg" alt=""></a>
+<img src="logo.jpeg" alt="">
 </div>
 <div id="search">
-
-<form action="" class="search_bar">
-    <input  type="text" id="search-2" placeholder="search for product" >
-    <button>Search</button>
-    
-
 <form action="" id="search-form">
     <input type="text" placeholder="search for product" id=form-search>
-    <input type="submit">
-
+    <button type="submit">Search</button>
 </form>
 </div>
 
@@ -29,24 +22,12 @@ let nav =
 <div id="login">
 <div>
 
-   
-    <a href="">Login/SignUp</a>
-    
-    
-
-
     <a href="signup.html" id="rlogin" >Login/SignUp</a>
-
 
 </div>
 
-
-<div id="basket" onclick="window.location.href='cart1.html'">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
-
 <div>
     <img id = "cart_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
-
         alt="">
     <h3>My Basket</h3>
     <h3 id="cart_count">0</h3>
@@ -54,30 +35,5 @@ let nav =
 </div>
 
 `
-// 
+
 export default nav
-
-// let searchinput = document.getElementById("search-2")
-
-// // let api = 
-
-// async function apicall(){
-//     let  search =searchinput.value;
-
-//     if(search !==""){
-//         let api = (`https://macho-hair-backend.vercel.app/${search}`);
-//         try {
-//             let req = await fetch(api);
-//             let data = req.json();
-    
-//             console.log(data)
-            
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-       
-
-// }
-// apicall()
-
