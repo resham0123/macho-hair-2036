25d34619b0fb5255d914b4c5755e56f0d77da7bf
Mahendra <mahendrac121@gmail.com>
2023-02-26 21:12:37 +0530

    addded iter 6

--- navbar.js
+++ navbar.js
@@ -6,9 +6,7 @@ let nav =
 </div>
 <div id="search">
 
-<form action="" class="search_bar">
-    <input  type="text" id="search-2" placeholder="search for product" >
-    <button>Search</button>
+
     
 
 <form action="" id="search-form">
@@ -42,7 +40,6 @@ let nav =
 
 
 <div id="basket" onclick="window.location.href='cart1.html'">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
 
 <div>
     <img id = "cart_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
@@ -57,27 +54,5 @@ let nav =
 // 
 export default nav
 
-// let searchinput = document.getElementById("search-2")
 
-// // let api = 
-
-// async function apicall(){
-//     let  search =searchinput.value;
-
-//     if(search !==""){
-//         let api = (`https://macho-hair-backend.vercel.app/${search}`);
-//         try {
-//             let req = await fetch(api);
-//             let data = req.json();
-    
-//             console.log(data)
-            
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-       
-
-// }
-// apicall()
 
