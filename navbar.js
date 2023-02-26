412ae44f6a93b36b3829f03c6f9ccdd82f4428ae
kirtisule <kirtisule27111996@gmail.com>
2023-02-26 22:19:43 +0530

    added but don't push

--- navbar.js
+++ navbar.js
@@ -31,9 +31,7 @@ let nav =
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
 
 <div>
-    <img id = "cart_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJIqso8C1IWNREoH1G489UcD8PvgT1jRzqeAjFDlJ0xpVzEuUP1c1mj4Og0EwjS4GgmBM&usqp=CAU"
-
-        alt="">
+    
     <h3>My Basket</h3>
     <h3 id="cart_count">0</h3>
 </div>
